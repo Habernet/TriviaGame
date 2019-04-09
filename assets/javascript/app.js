44b79661ce35588c84b68a4011a652585290e123
Patrick Haberern <patrick@Patricks-MacBook-Pro.local>
2019-04-09 17:12:52 -0400

    Added function clearTestArea() which empties all the divs on the page. 	Things still broken: 	1. Timeouts for loading the next question after three seconds. 	2. The If/Else statement to test if the button clicked is the right answer. 	3. A way to test if you are on the last question, in order to call gameOver().

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,25 +4,25 @@ questions = [
         question: "What year did World War 2 start?",
         answers: [1918, 1912, 1945, 1939],
         answer: 1939,
-        IMGUrl: "URL HERE"
+        IMGUrl: "assets/images/ww2map.png"
     },
     {
         question: "What was the most produced tank of the war?",
         answers: ["M4 Sherman", "Panzer MkVI", "T34", "The Tiger"],
         answer: "T34",
-        IMGUrl: "URL HERE"
+        IMGUrl: "assets/images/T34.jpg"
     },
     {
         question: "What year did America join the war?",
         answers: [1939, 1941, 1944, 1945],
         answer: 1941,
-        IMGUrl: "URL HERE"
+        IMGUrl: "assets/images/ww2map.png"
     },
     {
         question: "What is the date for VE day? (Victory in Europe)",
         answers: ["1918", "1912", "1945", "1939"],
         answer: "May 8th, 1945",
-        IMGUrl: "URL HERE"
+        IMGUrl: "assets/images/VEday.jpg"
     },
     {
         question: "What was the name of the plutonium bomb used by America?",
@@ -42,37 +42,36 @@ var timer = 10;
 var currentQuestion;
 var correct = 0;
 var incorrect = 0;
-var currentQuestionNumber= 0;
+var currentQuestionNumber = 0;
 
 // Global Functions
-var loadQuestion = function (questionobject) {
-    // set this question to the current question variable
-    currentQuestion = questionobject;
-    // empty the test area if there is anything there
+var clearTestArea = function(){
     $("#answers").empty();
     $("#correct-image").empty();
     $("#question-true-false").empty();
-    // uses Jquery to fill the respective DIVs on the page
+    $("#timer").text("");
+}
+var loadQuestion = function (questionobject) {
+    currentQuestion = questionobject;
+    clearTestArea();
     $("#question-true-false").text(questionobject.question);
     for (let i = 0; i < questionobject.answers.length; i++) {
         $("#answers").append('<button class="answerbutton" data-answer="' + questionobject.answers[i] + '">' + questionobject.answers[i] + '</button>')
     }
-    // timer on the page
     $("#timer").text(timer);
-    // start the timer
     countdown = setInterval(pageTimer, 1000);
 }
 
 var timesUp = function () {
     clearInterval(countdown);
-    //reinitialize the timer
     timer = 10;
     currentQuestionNumber++;
+    clearTestArea();
     $("#question-true-false").text("Time's up! The correct answer is: " + currentQuestion.answer);
     $("#correct-image").html('<img src ="' + currentQuestion.IMGUrl + '">');
-    next = setTimeout(loadQuestion(questions[currentQuestionNumber]), 3000);
-    // this runs immediately. does no wait 3 seconds
+    setTimeout(loadQuestion(questions[currentQuestionNumber]), 3000);
 }
+
 var pageTimer = function () {
     timer--;
     $("#timer").html(timer);
@@ -83,34 +82,31 @@ var pageTimer = function () {
 }
 
 var chosenCorrectly = function () {
-    //clear the test area
     currentQuestionNumber++;
+    correct++;
     clearInterval(countdown);
+    clearTestArea();
     $("#question-true-false").text("Correct!");
     $("#correct-image").html('<img src ="' + currentQuestion.IMGUrl + '">');
-    next = setTimeout(loadQuestion(questions[currentQuestionNumber]), 3000);
+    next = set
+    Timeout(loadQuestion(questions[currentQuestionNumber]), 3000);
 }
 var chosenIncorrectly = function () {
-    // clear the test area
     currentQuestionNumber++;
+    incorrect++;
     clearInterval(countdown);
+    clearTestArea();
     $("#question-true-false").text("Incorrect! The correct answer is: " + currentQuestion.answer);
     $("#correct-image").html('<img src ="' + currentQuestion.IMGUrl + '">');
     next = setTimeout(loadQuestion(questions[currentQuestionNumber]), 3000);
-
 }
 
 var gameOver = function () {
-    // this will hide everything on screen and show how many were correct/incorrect etc.
-
+    clearTestArea();
+    $("#timer").remove();
+    var results = "Game over! You answered " + correct + "/" + incorrect + " correctly!"
+    $("#header-area").append("<h2>" + results + "</h2>");
 }
-
-// Game logic PsuedoCode
-// High level
-// document onready()
-// Page loads...Only thing showing is Start button. 
-// Start button on click...gameActive = true, hide start button
-// load a question1 (timer starts)
 
 $(document).ready(function () {
     console.log("page loaded");
@@ -122,15 +118,12 @@ $(document).ready(function () {
     $(document).on("click", ".answerbutton", function (event) {
         console.log("buttonworks");
         console.log("Answer: " + currentQuestion.answer);
-        // if ($(event.target).data("name") == '"' + currentQuestion.answer + '"') {
-        //     console.log("this works");
-        //     // Right answer, load next question
-        //     chosenCorrectly();
-
-        // } else {
-        //     console.log("this works too");
-        //     // wrong answer, load next question
-        //     chosenIncorrectly();
-        // }
+        if ($(event.target).data("name") === '"' + currentQuestion.answer + '"') {
+            console.log("chose correctly!");
+            chosenCorrectly();
+        } else {
+            console.log("chose incorrectly!");
+            chosenIncorrectly();
+        }
     })
 })
